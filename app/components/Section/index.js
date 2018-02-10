70ec7ef73b6946836a38b7860388b10237892aa1
kevinprakasa <kevinprakasa03@gmail.com>
2018-02-10 20:40:21 +0700

    add component section

---
+++ app/components/Section/index.js
@@ -0,0 +1,24 @@
+/**
+*
+* Section
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+
+class Section extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+Section.propTypes = {
+
+};
+
+export default Section;
